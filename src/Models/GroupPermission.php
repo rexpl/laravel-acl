b2f5a4be1e761ac1a7d2cc830aa8856ceebc8bf4
rexpl <75545403+rexpl@users.noreply.github.com>
2022-10-29 14:41:20 +0200

    Fixed GroupPermission model relationship keys

--- src/Models/GroupPermission.php
+++ src/Models/GroupPermission.php
@@ -39,6 +39,6 @@ class GroupPermission extends Model
      */
     public function permission()
     {
-        return $this->belongsTo(Permisson::class, 'permission_id', 'id');
+        return $this->belongsTo(Permission::class, 'permission_id', 'id');
     }
 }
