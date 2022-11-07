3bf3102afded5895e208449f0bc5926e1989e94b
rexpl <75545403+rexpl@users.noreply.github.com>
2022-11-07 10:15:24 +0100

    Refactor user and group

---
+++ src/BaseGroup.php
@@ -0,0 +1,318 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rexpl\LaravelAcl;
+
+use Illuminate\Support\Collection;
+use Rexpl\LaravelAcl\Exceptions\ResourceNotFoundException;
+use Rexpl\LaravelAcl\Models\{
+    Group as GroupModel,
+    ParentGroup,
+    GroupPermission,
+    Permission,
+    GroupDependency,
+    GroupUser
+};
+
+abstract class BaseGroup
+{
+    /**
+     * @var GroupModel|null
+     */
+    protected $group = null;
+
+
+    /**
+     * Returns the group model.
+     * 
+     * @return GroupModel
+     */
+    protected function group(): GroupModel
+    {
+        return $this->group ?? $this->fetchGroupModel();
+    }
+
+
+    /**
+     * Returns the group model.
+     * 
+     * @return GroupModel
+     */
+    abstract protected function fetchGroupModel(): GroupModel;
+
+
+    /**
+     * Return all permissions.
+     * 
+     * @return Collection
+     */
+    public function groupPermissions(): Collection
+    {
+        return GroupPermission::where('group_id', $this->group()->id)->get()->pluck('permission');
+    }
+
+
+    /**
+     * Adds a permission to the group.
+     * 
+     * @param Permission|string|int $permission
+     * 
+     * @return void
+     */
+    public function addPermission(Permission|string|int $permission): void
+    {
+        $permission = $this->fetchPermission($permission);
+
+        $record = new GroupPermission();
+
+        $record->permission_id = $permission->id;
+        $record->group_id = $this->group()->id;
+
+        $record->save();
+    }
+
+
+    /**
+     * Removes a permission from the group.
+     * 
+     * @param Permission|string|int $permission
+     * 
+     * @return void
+     */
+    public function removePermission(Permission|string|int $permission): void
+    {
+        $permission = $this->fetchPermission($permission);
+
+        GroupPermission::where('permission_id', $permission->id)
+            ->where('group_id', $this->group()->id)
+            ->delete();
+    }
+
+
+    /**
+     * Fetches the permission model to be sure it exist
+     * 
+     * @param Permission|string|int $permission
+     * 
+     * @return Permission
+     */
+    protected function fetchPermission(Permission|string|int $permission): Permission
+    {
+        if (!$permission instanceof Permission) {
+
+            $result = is_numeric($permission)
+                ? Permission::find($permission)
+                : Permission::firstWhere('name', $permission);
+
+            if (null === $result) {        
+                throw new ResourceNotFoundException(
+                    'Permission ' . $permission . ', not found.'
+                );
+            }
+
+            $permission = $result;
+        }        
+
+        return $permission;
+    }
+
+
+    /**
+     * Return all the parent groups.
+     * 
+     * @return Collection
+     */
+    public function parentGroups(): Collection
+    {
+        return ParentGroup::where('child_id', $this->group()->id)->get()->pluck('parent');
+    }
+
+
+    /**
+     * Add parent group.
+     * 
+     * @param Group|int $group
+     * 
+     * @return void
+     */
+    public function addParentGroup(Group|int $group): void
+    {
+        if (is_int($group)) $group = static::getGroupByID($group);
+
+        $record = new ParentGroup();
+
+        $record->child_id = $this->group()->id;
+        $record->parent_id = $group->id();
+
+        $record->save();
+    }
+
+
+    /**
+     * Remove parent group.
+     * 
+     * @param Group|int $group
+     * 
+     * @return void
+     */
+    public function removeParentGroup(Group|int $group): void
+    {
+        if (is_int($group)) $group = static::getGroupByID($group);
+
+        ParentGroup::where('child_id', $this->group()->id)
+            ->where('parent_id', $group->id())
+            ->delete();
+    }
+
+
+    /**
+     * Returns all the child groups
+     * 
+     * @return Collection
+     */
+    public function childGroups(): Collection
+    {
+        return ParentGroup::where('parent_id', $this->group()->id)->get()->pluck('child');
+    }
+
+
+    /**
+     * Add parent group.
+     * 
+     * @param Group|int $group
+     * 
+     * @return void
+     */
+    public function addChildGroups(Group|int $group): void
+    {
+        if (is_int($group)) $group = static::getGroupByID($group);
+
+        $record = new ParentGroup();
+
+        $record->child_id = $group->id();
+        $record->parent_id = $this->group()->id;
+
+        $record->save();
+    }
+
+
+    /**
+     * Remove parent group.
+     * 
+     * @param Group|int $group
+     * 
+     * @return void
+     */
+    public function removeChildGroups(Group|int $group): void
+    {
+        if (is_int($group)) $group = static::getGroupByID($group);
+
+        ParentGroup::where('child_id', $group->id())
+            ->where('parent_id', $this->group()->id)
+            ->delete();
+    }
+
+
+    /**
+     * Deletes the group.
+     * 
+     * @param bool $clean
+     * 
+     * @return void
+     */
+    public function destroy(bool $clean = true): void
+    {
+        static::delete($this->group()->id, $clean);
+    }
+
+
+    /**
+     * Get the group model by ID.
+     * 
+     * @param int $id
+     * 
+     * @return GroupModel
+     */
+    protected static function getGroupByID(int $id): GroupModel
+    {
+        $group = GroupModel::find($id);
+
+        if (null === $group) {        
+            throw new ResourceNotFoundException(
+                'Group with id: ' . $id . ', not found.'
+            );
+        }
+
+        return $group;
+    }
+
+
+    /**
+     * Get the group model by user ID.
+     * 
+     * @param int $id
+     * 
+     * @return GroupModel
+     */
+    protected static function getGroupByUserID(int $id): GroupModel
+    {
+        $group = GroupModel::firstWhere('user_id', $id);
+
+        if (null === $group) {        
+            throw new ResourceNotFoundException(
+                'Group with user id: ' . $id . ', not found.'
+            );
+        }
+
+        return $group;
+    }
+
+
+    /**
+     * Cleans related data to group.
+     * 
+     * @param int $id
+     * 
+     * @return void
+     */
+    protected static function cleanGroup(int $id): void
+    {
+        GroupUser::where('group_id', $id)->delete();
+        GroupDependency::where('group_id', $id)->delete();
+        GroupPermission::where('group_id', $id)->delete();
+        ParentGroup::where('child_id', $id)
+            ->orWhere('parent_id', $id)->delete();
+    }
+
+
+    /**
+     * Creates new group.
+     * 
+     * @param string|int $nameORuserId
+     * 
+     * @return static
+     */
+    abstract public static function new(string|int $nameORuserId): static;
+
+
+    /**
+     * Deletes the group by ID.
+     * 
+     * @param int $id
+     * @param bool $clean
+     * 
+     * @return void
+     */
+    abstract public static function delete(int $id, bool $clean = true): void;
+
+
+    /**
+     * Returns the group instance.
+     * 
+     * @param int $id
+     * 
+     * @return static
+     */
+    abstract public static function find(int $id): static;
+}
