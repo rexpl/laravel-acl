dbdd8c77ebc938a4c09a3c18d07e9ee19ce21b8a
rexpl <75545403+rexpl@users.noreply.github.com>
2022-12-26 16:02:14 +0100

    Refactor: add docblock to config file

--- config/acl.php
+++ config/acl.php
@@ -4,33 +4,45 @@ return [
 
     /**
      * Iteration count. (see documentation)
+     * 
+     * @var int
      */
     'nFactor' => 3,
 
     /**
      * Automatically make gates for each permission.
+     * 
+     * @var bool
      */
     'gates' => false,
 
     /**
      * Cache user's acl. (highly recommended)
+     * 
+     * @var bool
      */
     'cache' => true,
 
     /**
      * How long to cache each users acl info.
      * Duration: seconds
+     * 
+     * @var int
      */
     'duration' => 604800, // 1 week
 
     /**
      * Cache records acl.
+     * 
+     * @var bool
      */
     'record_cache' => true,
 
     /**
      * How long to cache each records acl info.
      * Duration: seconds
+     * 
+     * @var int
      */
     'record_duration' => 300, // 5 minutes
 
