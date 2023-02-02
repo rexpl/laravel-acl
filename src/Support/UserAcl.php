b810de07d1ac3148400b334c59aafe7c8c4e9cb5
rexpl <75545403+rexpl@users.noreply.github.com>
2023-02-02 15:26:19 +0100

    Feature: trait to add to user model

---
+++ src/Support/UserAcl.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rexpl\LaravelAcl\Support;
+
+use Rexpl\LaravelAcl\Facades\Acl;
+use Rexpl\LaravelAcl\User;
+
+trait UserAcl
+{
+    /**
+     * Returns the acl instance of the user.
+     * 
+     * @return \Rexpl\LaravelAcl\User
+     */
+    public function acl(): User
+    {
+        return Acl::user($this->getAuthIdentifier());
+    }
+}
